2401d93c8f895ef7596602c1adceb709fb843495
NicolasEcheverri <nicolas.echeverrir@udea.edu.co>
2024-02-14 22:50:38 -0500

    agregando loggers y utils

---
+++ src/cnnClassifier/__init__.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import logging
+
+#modulo: carpeta de donde se está ejecutando. 
+#message es el mensaje que se le pasa  logger.info("welcome to cnnClassifier") 
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+#ejemplo de salida: [2024-02-14 22:13:45,905: INFO: main: welcome to cnnClassifier]
+log_dir = "logs"
+log_filepath = os.path.join(log_dir,"running_logs.log")
+#creando la carpeta llamada log_dir
+os.makedirs(log_dir, exist_ok=True)
+
+
+logging.basicConfig(
+    level= logging.INFO,
+    format= logging_str,
+
+    handlers=[
+        logging.FileHandler(log_filepath),
+        logging.StreamHandler(sys.stdout) #imprime el log 
+    ]
+)
+
+logger = logging.getLogger("cnnClassifierLogger")
---
+++ templates/main.py
@@ -0,0 +1,4 @@
+from cnnClassifier import logger
+
+logger.info("welcome to cnnClassifier")
+#ejemplo de salida: [2024-02-14 22:13:45,905: INFO: main: welcome to cnnClassifier]
